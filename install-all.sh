f2b8600938ead2fa2649b9a3c12a773559e29628
Pavel Tsandev <p.candev@gmail.com>
2023-10-04 03:23:25 +0300

    Add script to install all dependencies for all apps

---
+++ install-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directories to install dependencies in
+directories=("Client" "ConnectServer" "GameServer", "JoinServer", "libs/mu-db", "libs/mu-logger", "libs/mu-packet-manager", "WebServer/mu-socket-server", "WebServer/mu-web-admin")
+
+# Install dependencies in each directory
+for dir in "${directories[@]}"; do
+  echo "Installing dependencies in $dir..."
+  yarn --cwd "$dir" install
+done
+
+# Install dependencies in the root directory
+echo "Installing dependencies in the root directory..."
+yarn install
+
+echo "All dependencies have been installed."
