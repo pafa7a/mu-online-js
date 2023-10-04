34536c4b33cb7456b5b94864abd35d4953988f69
Pavel Tsandev <p.candev@gmail.com>
2023-10-05 01:20:56 +0300

    Fix the bash script for reinstalling node dependencies on all projects.

--- install-all.sh
+++ install-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Directories to install dependencies in
-directories=("Client" "ConnectServer" "GameServer", "JoinServer", "libs/mu-db", "libs/mu-logger", "libs/mu-packet-manager", "WebServer/mu-socket-server", "WebServer/mu-web-admin")
+directories=("Client" "ConnectServer" "GameServer" "JoinServer" "libs/mu-db" "libs/mu-logger" "libs/mu-packet-manager" "WebServer/mu-socket-server" "WebServer/mu-web-admin")
 
 # Install dependencies in each directory
 for dir in "${directories[@]}"; do
   echo "Installing dependencies in $dir..."
+  rm -rf "${dir}/node_modules";
   yarn --cwd "$dir" install
 done
 
